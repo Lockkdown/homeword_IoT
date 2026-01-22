bf1558ba4e45e788505b20f1878c40f11fceb0ea
Lockdown <hoangphuc15072004@gmail.com>
2026-01-22 21:03:30 +0700

    doing lab301, lab302

---
+++ Lab3/lab301/src/main/java/com/truonghoangphuc/lab301/Lab301Application.java
@@ -0,0 +1,13 @@
+package com.truonghoangphuc.lab301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab301Application.class, args);
+	}
+
+}
---
+++ Lab3/lab302/android/app/src/main/java/com/truonghoangphuc/lab302/MainActivity.java
@@ -0,0 +1,6 @@
+package com.truonghoangphuc.lab302;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
