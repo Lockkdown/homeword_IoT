10ac96472e16b8fdb0f59ef51b642b31542f8dc3
Lockdown <hoangphuc15072004@gmail.com>
2026-01-26 21:19:26 +0700

    doing Lab305

---
+++ Lab3/lab305/src/main/java/com/truonghoangphuc/lab305/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.truonghoangphuc.lab305.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.truonghoangphuc.lab305.entity.Category;
+
+public interface CategoryRepository extends JpaRepository< Category, Long> {
+}
