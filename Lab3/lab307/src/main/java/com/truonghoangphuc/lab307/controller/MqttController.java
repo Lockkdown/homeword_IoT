e59972e6c608d7f003560e15f4f12bb476dc0529
Lockdown <hoangphuc15072004@gmail.com>
2026-01-28 21:01:34 +0700

    doing Lab307(but have a BUG)

---
+++ Lab3/lab307/src/main/java/com/truonghoangphuc/lab307/controller/MqttController.java
@@ -0,0 +1,21 @@
+package com.truonghoangphuc.lab307.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.truonghoangphuc.lab307.service.MqttPublisherService;
+
+@RestController
+@RequestMapping("/api/mqtt")
+public class MqttController {
+    private final MqttPublisherService mqttPublisherService;
+    public MqttController(MqttPublisherService mqttPublisherService) {
+        this.mqttPublisherService = mqttPublisherService;
+    }
+    @GetMapping("/publish")
+    public String publish() {
+        mqttPublisherService.publish("Hi from the IoT application");
+        return "Message published!";
+    }
+}
