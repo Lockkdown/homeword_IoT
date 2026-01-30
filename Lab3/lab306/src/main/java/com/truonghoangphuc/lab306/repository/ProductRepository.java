d04f1b8dd193d8fc24af0018560ec131549e8cca
Lockdown <hoangphuc15072004@gmail.com>
2026-01-30 13:28:12 +0700

    chang version lab304: 4.2.0 -> 3.5.10

--- Lab3/lab306/src/main/java/com/truonghoangphuc/lab306/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.truonghoangphuc.lab306.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.truonghoangphuc.lab306.entity.Product;
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
